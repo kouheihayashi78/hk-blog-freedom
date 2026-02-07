98125cfac2b52f6ceac429c178f9eaf3a9da77d9
kouhei <kouhei.basketball7.8@gmail.com>
2026-02-07 18:28:22 +0900

    add - データ取得関数、型定義ファイル実装

---
+++ src/types/blog.ts
@@ -0,0 +1,33 @@
+import type { MicroCMSDate, MicroCMSImage } from "microcms-js-sdk";
+
+export type Tag = {
+  id: string;
+  tagName: string;
+} & MicroCMSDate;
+
+export type BlogPost = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch?: MicroCMSImage;
+  tags: Tag[];
+  isPinned: boolean;
+  publishedAt: string;
+  revisedAt: string;
+  createdAt: string;
+  updatedAt: string;
+} & MicroCMSDate;
+
+export type BlogPostListResponse = {
+  contents: BlogPost[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+};
+
+export type TagListResponse = {
+  contents: Tag[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+};
