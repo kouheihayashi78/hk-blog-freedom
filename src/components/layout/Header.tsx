a406d9b018c059290b815e639d955ca9650959b7
kouhei <kouhei.basketball7.8@gmail.com>
2026-02-15 22:29:12 +0900

    add - aboutページを実装

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -36,6 +36,11 @@ export const Header = ({ tags }: { tags: Tag[] }) => {
       </p>
 
       <div className="absolute top-0 right-0">
+        <Link href='/about' className="inline-flex p-2 text-gray-600 hover:text-gray-900 transition-colors" aria-label="About">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+          </svg>
+        </Link>
         <button
           onClick={() => setIsSearchOpen(!isSearchOpen)}
           className="p-2 text-gray-600 hover:text-gray-900 transition-colors"
